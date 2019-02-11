2555e5f96e8a9853ec943d29a53ac4d87123ad87
kamilkrol95 <kamilkrol95@gmail.com>
2019-02-11 20:52:52 +0100

    Add application files

---
+++ PhotoTinderController/LoadImages.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+
+namespace PhotoTinderController
+{
+    public class LoadImages : ILoadImages
+    {
+
+        private static Queue<String> PicturesPathsQueue;
+        private static String FolderPath;        
+        public static void Main()
+        {
+
+        }
+        public BitmapImage LoadNextImage(Queue<String> ImagesQueue)
+        {
+            var src = new BitmapImage();
+
+            if (ImagesQueue.Count > 0)
+            {
+                var imagePath = ImagesQueue.Peek();
+
+                if (File.Exists(imagePath))
+                {
+                    src.BeginInit();
+                    src.UriSource = new Uri(ImagesQueue.Peek(), UriKind.Absolute);
+                    src.EndInit();
+
+                    return src;
+                }
+            }
+            return null;
+        }
+        public BitmapImage CopyImageToDir(String folderName)
+        {
+
+            if (PicturesPathsQueue.Count > 0)
+            {
+                var fileToCopy = PicturesPathsQueue.Peek();
+                PicturesPathsQueue.Dequeue();
+                var fileName = fileToCopy.Substring(FolderPath.Length);
+                var newFolderPath = FolderPath + folderName;
+
+                if (Directory.Exists(newFolderPath))
+                {
+                    if (File.Exists(fileToCopy))
+                    {
+                        File.Copy(fileToCopy, newFolderPath + fileName, true);
+                        Console.WriteLine(fileName + " copied!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such file!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No such directory!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No more photos!");
+            }
+            return LoadNextImage(PicturesPathsQueue);
+        }
+
+        public BitmapImage DeleteImage()
+        {
+            if (PicturesPathsQueue.Count > 0)
+            {
+                PicturesPathsQueue.Dequeue();
+            }
+
+            return LoadNextImage(PicturesPathsQueue);
+        }
+        
+        public BitmapImage AddImages()
+        {
+
+            FolderBrowserDialog folderPathDialog = new FolderBrowserDialog();
+
+            if (folderPathDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                String folderPath = folderPathDialog.SelectedPath;
+                FolderPath = folderPath;
+                
+                var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+                var files = GetFilesFrom(folderPath, filters);
+                WritePaths(files);
+
+                PicturesPathsQueue = ListToQueue(files);
+                Directory.CreateDirectory(folderPath + "/all");
+                Directory.CreateDirectory(folderPath + "/chosen");
+               
+                return LoadNextImage(ListToQueue(files));                
+            }
+            return null;
+        }
+        private static List<String> GetFilesFrom(String searchFolder, String[] filters)
+        {
+            List<String> picturesFound = new List<String>();
+            foreach (var filter in filters)
+            {
+                picturesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter)));
+            }
+            return picturesFound;
+        }
+
+        private static void WritePaths(List<String> list)
+        {
+            foreach (var p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+        private static Queue<String> ListToQueue(List<String> list)
+        {
+            return new Queue<string>(list);
+        }
+    }
+}
---
+++ PhotoTinderView/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Input;
+using PhotoTinderController;
+
+namespace PhotoTinderView
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Funkcja dodawania wszsytkich zdjec z folderu                     OK
+        // Dodanie eventow od klawiszy                                      OK
+        // Dodanie menu rozwijanego z dodawaniem sciezki (opcjonalnie)      OK
+        // Funkcja kopiujaca zdjecia do podfolderow                         OK
+        // Funkcja usuwajaca zdjecie    
+        // Sprobowac zamienic klase na interfejs w MainWindow               OK
+
+        public PhotoTinderController.ILoadImages _loadImages;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _loadImages = new LoadImages();
+            this.KeyDown += new System.Windows.Input.KeyEventHandler(MainWindow_KeyDown);
+        }
+
+        private void Button_Delete_Photo(object sender, RoutedEventArgs e)
+        {
+            Delete();
+        }
+
+        private void Button_Move_Chosen_Photo(object sender, RoutedEventArgs e)
+        {
+            MoveChosen();
+        }
+
+        private void Button_Move_All_Photo(object sender, RoutedEventArgs e)
+        {
+            MoveAll();
+        }
+
+        private void Button_Load_Photo(object sender, RoutedEventArgs e)
+        {
+            LoadContent();
+        }
+
+        private void LoadFolder_Click(object sender, RoutedEventArgs e)
+        {
+            LoadContent();
+            
+        }
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Right)
+            {
+                MoveAll();
+            }
+            if (e.Key == Key.Left)
+            {
+                MoveChosen();
+            }
+            if (e.Key == Key.Down)
+            {
+                Delete();
+            }
+            if (e.Key == Key.Up)
+            {
+                LoadContent();
+            }
+        }
+
+        public void ShowImage(BitmapImage imageSource)
+        {
+            ImageFrame.Source = imageSource;
+        }
+        
+        public void LoadContent()
+        {
+            BitmapImage imageSource = _loadImages.AddImages();
+            if (imageSource != null)
+            {
+                InformationLabel.Content = "";
+            }
+            else
+            {
+                InformationLabel.Content = "No images";                
+            }
+            ShowImage(imageSource);
+        }
+
+        public void MoveAll()
+        {
+            BitmapImage imageSource = _loadImages.CopyImageToDir("/all");
+            if (imageSource != null)
+            {
+                InformationLabel.Content = "";
+            }
+            else
+            {
+                InformationLabel.Content = "No images";                
+            }
+            ShowImage(imageSource);
+        }
+
+        public void MoveChosen()
+        {
+            BitmapImage imageSource = _loadImages.CopyImageToDir("/chosen");
+            if (imageSource != null) 
+            {
+                InformationLabel.Content = "";
+            }
+            else
+            {
+                InformationLabel.Content = "No images";                
+            }
+            ShowImage(imageSource);
+        }
+
+        public void Delete()
+        {
+            BitmapImage imageSource = _loadImages.DeleteImage();
+            if (imageSource != null)
+            {
+                InformationLabel.Content = "";
+            }
+            else
+            {
+                InformationLabel.Content = "No images";               
+            }
+            ShowImage(imageSource);
+        }
+    }
+}
